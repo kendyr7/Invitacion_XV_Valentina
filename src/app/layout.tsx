db03f988d2a20bc491945106fbecec5288ba93d3
Kendyr Quintanilla <kendyr7@gmail.com>
2025-06-21 01:22:45 +0000

    Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Invitacion XV Valentina',
